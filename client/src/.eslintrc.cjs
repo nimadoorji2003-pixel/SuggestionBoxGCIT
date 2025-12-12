1bf08498b9ff2dc2eb62a217de4ee5a284ff6cd3
unknown <12220048.gcit@rub.edu.bt>
2025-12-12 10:06:36 +0600

    Use classic ESLint config for client

--- client/src/.eslintrc.cjs
+++ client/src/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true, // for testing-library / vitest DOM stuff
   },
   extends: [
     "eslint:recommended",
