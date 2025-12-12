6b2dc5eb88e9ce21c88a22fcc5ddf050e82b8734
unknown <12220048.gcit@rub.edu.bt>
2025-12-12 09:55:31 +0600

    Switch ESLint to classic eslintrc to fix CI lint step

---
+++ client/src/.eslintrc.cjs
@@ -0,0 +1,29 @@
+// client/.eslintrc.cjs
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+  },
+  ignorePatterns: ["node_modules/", "dist/", "coverage/"],
+};
